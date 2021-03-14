8d0db901489c3823c383b0ab6e45088c01dcb11c
Francesco <goras.francesco@gmail.com>
2021-03-14 16:14:58 +1300

    Added RTT_CHECK (ping)

--- index.js
+++ index.js
@@ -8,40 +8,42 @@ const server = new Net.createServer();
 const connectedSockets = new Map();
 
 // broadcast to all connected sockets except one
-connectedSockets.broadcast = function(data, except) {
+connectedSockets.broadcast = function (data, except) {
     for (const [key, value] of connectedSockets.entries()) {
         if (key !== except) {
-            key.write(except.remoteAddress+":"+except.remotePort+" "+data);
+            key.write(except.remoteAddress + ":" + except.remotePort + " " + data);
         }
     }
 };
 
-server.listen(port, "0.0.0.0",function() {
+server.listen(port, "0.0.0.0", function () {
     console.log(`Server listening for connection requests on socket localhost:${port}`);
     console.log(server.address());
 });
 
-server.on('connection', function(socket) {
-    console.log("Client: " + socket.remoteAddress+":"+socket.remotePort);
+server.on('connection', function (socket) {
+    console.log("Client: " + socket.remoteAddress + ":" + socket.remotePort);
     connectedSockets.set(socket, undefined); //add user to cache
 
     //user passes their position, every other user receives their position
-    socket.on('data', function(chunk) {
+    socket.on('data', function (chunk) {
+        if (chunk.toString().includes("RTT_CHECK"))
+            socket.write("RTT_CHECK")
         //store user position
-        connectedSockets.set(socket, socket.remoteAddress+":"+socket.remotePort+"="+chunk.toString());
+        connectedSockets.set(socket, socket.remoteAddress + ":" + socket.remotePort + "=" + chunk.toString());
         //broadcast to other users this user's position
         connectedSockets.broadcast(chunk, socket);
         console.log(connectedSockets.get(socket));
     });
 
-    socket.on('end', function() {
+    socket.on('end', function () {
         console.log('Closing connection with the client');
-	connectedSockets.delete(socket);
-	socket.end();
+        connectedSockets.delete(socket);
+        socket.end();
     });
 
-    socket.on('error', function(err) {
+    socket.on('error', function (err) {
         console.log(`${err}`);
-	socket.end();
+        socket.end();
     });
 });
